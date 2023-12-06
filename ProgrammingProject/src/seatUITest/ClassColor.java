e63c83c70648c45742446252042c09bc6e7fc420
user <user@DESKTOP-31IHISG>
2023-12-06 18:40:16 +0900

    등급별 색,

---
+++ ProgrammingProject/src/seatUITest/ClassColor.java
@@ -0,0 +1,174 @@
+package seatUITest;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class ClassColor extends JFrame {
+    private MyPanel panel = new MyPanel();
+    private JLabel selectionLabel = new JLabel("Selected Grade: None, Panel Number: None");
+    private int panelsToSelect = 0;
+    private int remainingPanels;
+    private String selectedGrade = null;
+    private int[] selectedPanelNumbers = new int[2]; // Store panel numbers for the selected panels
+    private int selectedPanelsCount = 0; // Counter for the selected panels
+    private Color[][] originalColors;
+
+    public ClassColor() {
+        setTitle("JPanel ex");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Prompt the user to choose the number of panels to select (1 or 2)
+        while (true) {
+            String input = JOptionPane.showInputDialog(this, "Enter the number of panels to select (1 or 2):");
+            try {
+                panelsToSelect = Integer.parseInt(input);
+                if (panelsToSelect >= 1 && panelsToSelect <= 2) {
+                    break;
+                } else {
+                    JOptionPane.showMessageDialog(this, "Invalid input. Please enter 1 or 2.");
+                }
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(this, "Invalid input. Please enter 1 or 2.");
+            }
+        }
+
+        remainingPanels = panelsToSelect;
+        originalColors = new Color[9][10];
+
+        setLayout(new BorderLayout());
+        add(selectionLabel, BorderLayout.SOUTH);
+        add(panel, BorderLayout.CENTER);
+
+        setPreferredSize(new Dimension(1000, 1000));
+        pack();
+        setVisible(true);
+    }
+
+    class MyPanel extends JPanel {
+        private Color[][] panelColors;
+
+        public MyPanel() {
+            panelColors = new Color[9][10]; // 9 rows (3 for each grade), 10 columns
+            initializePanelColors();
+            addMouseListener(new MyMouseAdapter());
+        }
+
+        private void initializePanelColors() {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 10; j++) {
+                    panelColors[i][j] = assignGradeColor(i, j);
+                }
+            }
+            // Save the original colors
+            originalColors = new Color[9][10];
+            for (int i = 0; i < 9; i++) {
+                System.arraycopy(panelColors[i], 0, originalColors[i], 0, 10);
+            }
+        }
+
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.setColor(new Color(0, 0, 0));
+
+            // Draw panels with different grades in separate rows
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 10; j++) {
+                    g.setColor(panelColors[i][j]);
+                    g.fillRect(110 + 80 * j, 160 + 80 * i, 60, 60);
+                }
+            }
+        }
+        
+        private Color assignGradeColor(int row, int column) {
+            // Assign colors based on the panel's position
+            int panelNumber = row * 10 + column + 1;
+            if (panelNumber <= 30) {
+                return panelColors[row][column] = new Color(255, 127, 50); // Set color for grade A
+            } else if (panelNumber <= 60) {
+                return panelColors[row][column] = new Color(0, 181, 226); // Set color for grade B
+            } else {
+                return panelColors[row][column] = new Color(255, 205, 0); // Set color for grade C
+            }
+        }
+
+        private class MyMouseAdapter extends MouseAdapter {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (remainingPanels > 0) {
+                    int column = (e.getX() - 110) / 80;
+                    int row = (e.getY() - 160) / 80;
+
+                    if (row >= 0 && row < 9 && column >= 0 && column < 10) {
+                        // Toggle the color of the clicked panel
+                        if (panelColors[row][column].equals(assignGradeColor(row, column))) {
+                            panelColors[row][column] = Color.red; // Change to white when clicked
+                        } else {
+                            panelColors[row][column] = assignGradeColor(row, column); // Change back to original color when clicked again
+                        }
+
+                        // Check if the grade is selected or select the grade
+                        if (selectedGrade == null) {
+                            selectedGrade = assignGrade(row, column);
+                            selectedPanelNumbers[selectedPanelsCount] = row * 10 + column + 1;
+                            selectedPanelsCount++;
+                        } else {
+                            String grade = assignGrade(row, column);
+                            if (!selectedGrade.equals(grade)) {
+                                JOptionPane.showMessageDialog(null, "Please select panels with the same grade.");
+                                // Revert the color change for the mistakenly selected panel
+                                panelColors[row][column] = assignGradeColor(row, column);
+                                return; // Do not decrease remainingPanels or continue if grades are different
+                            }
+                            selectedPanelNumbers[selectedPanelsCount] = row * 10 + column + 1;
+                            selectedPanelsCount++;
+                        }
+
+                        updateSelectionLabel();
+                        repaint(); // Trigger repaint to immediately update the GUI
+                        remainingPanels--;
+
+                        
+                    }
+                }
+            }
+
+            private void updateSelectionLabel() {
+                if (selectedPanelsCount == panelsToSelect) {
+                    selectionLabel.setText("Selected Grade: " + selectedGrade +
+                            ", Panel Numbers: " + selectedPanelNumbers[0] + ", " + selectedPanelNumbers[1]);
+                } else {
+                    selectionLabel.setText("Selected Grade: " + selectedGrade +
+                            ", Panel Number: " + selectedPanelNumbers[0]);
+                }
+                // Schedule the message dialog to ensure it appears after the label is updated
+                SwingUtilities.invokeLater(() -> {
+                    if (remainingPanels == 0) {
+                        JOptionPane.showMessageDialog(null, selectionLabel);
+                    }
+                });
+                
+                repaint();
+            }
+
+            private String assignGrade(int row, int column) {
+                // Assign grades based on the panel's position
+                int panelNumber = row * 10 + column + 1;
+                if (panelNumber <= 30) {
+                    return "A";
+                } else if (panelNumber <= 60) {
+                    return "B";
+                } else {
+                    return "C";
+                }
+            }
+
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        new ClassColor();
+    }
+}
+
